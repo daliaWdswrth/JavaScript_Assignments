bf5d976c61d6732bfd97e1cac0035908d5196fc8
daliaWdswrth <84307260+daliaWdswrth@users.noreply.github.com>
2021-05-16 23:35:56 -0700

    Add files via upload

---
+++ Assignment2.js
@@ -0,0 +1,28 @@
+//March 16, 2021
+//CS81
+//Dalia Wadsworth
+
+
+//loop Fizz for odd numbers
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 != 0) {
+        console.log("Fizz");
+    }
+    else {
+        console.log(i);
+    }
+}
+
+console.log("\n");
+
+//loop Buzz for even numbers
+for (let i = 10; i >= 1; i--) {
+    if (i % 2 == 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
+
+
